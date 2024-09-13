901f1f843cc9e120ae43a4f5938974cc04c71528
Aleksandr Solonitskii <sabmailcom@gmail.com>
2024-09-13 13:39:00 +0500

    Изменил настройки CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['https://kupitop.nomorepartiesco.ru/', 'http://localhost:3000/'],
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
